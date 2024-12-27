0473b4b836ad318248f0698542f566fec6b1ebff
Bwallker <benbwall@gmail.com>
2024-12-27 06:57:13 +0200

    Switch to using update style for calling set signal

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,7 +5,7 @@ const Counter = () => {
     return (
         <button
             class='w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]'
-            onClick={() => setCount(count() + 1)}
+            onClick={() => setCount((v) => v + 1)}
         >
             Clicks: {count()}
         </button>
