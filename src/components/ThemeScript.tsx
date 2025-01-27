ed86dada2461833deb786cb1ae7bab737e004c00
Bwallker <benbwall@gmail.com>
2025-01-27 23:46:16 +0200

    Add theme script props

--- src/components/ThemeScript.tsx
+++ src/components/ThemeScript.tsx
@@ -1,10 +1,12 @@
 const script =
-    '{let e=localStorage.getItem("theme");e=null!==e&&"system"!==e?e:window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light";document.documentElement.setAttribute("data-theme",e)}';
+    '{let e=localStorage.getItem("theme");e=null!==e&&"system"!==e?e:window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light";document.documentElement.setAttribute("data-theme",e);}';
 
-const ThemeScript = (props: { nonce?: string }) => (
-    <script id='theme-script' nonce={props.nonce}>
-        {script}
-    </script>
+export type ThemeScriptProps = {
+    nonce?: string;
+};
+
+const ThemeScript = (props: ThemeScriptProps) => (
+    <script nonce={props.nonce}>{script}</script>
 );
 
 export default ThemeScript;
