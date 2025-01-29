529f0f51ed1f164ed883fad621c2d6927f3befb4
Bwallker <benbwall@gmail.com>
2025-01-29 11:58:56 +0200

    Switch to using hex based nonce

--- src/util/nonce.ts
+++ src/util/nonce.ts
@@ -1,7 +1,9 @@
+import * as crypto from 'crypto';
+
 import { PageEvent } from '@solidjs/start/server';
 import { RequestEvent } from 'solid-js/web';
 
-export const generateNonce = () => crypto.randomUUID();
+export const generateNonce = () => crypto.randomBytes(16).toString('hex');
 
 export const getNonce = (event: PageEvent | RequestEvent | { nonce: string }) =>
     (event as unknown as { nonce: string }).nonce;
