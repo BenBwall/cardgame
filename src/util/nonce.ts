2aeb008704b702f58b0ceee23cd4c87223157e95
Bwallker <benbwall@gmail.com>
2025-01-30 07:02:49 +0200

    Fix Formatting

--- src/util/nonce.ts
+++ src/util/nonce.ts
@@ -1,9 +1,11 @@
-import * as crypto from 'crypto';
-
 import { PageEvent } from '@solidjs/start/server';
+import * as crypto from 'crypto';
 import { RequestEvent } from 'solid-js/web';
 
-export const generateNonce = () => crypto.randomBytes(16).toString('hex');
+const NONCE_SIZE = 16;
+
+export const generateNonce = () =>
+    crypto.randomBytes(NONCE_SIZE).toString('hex');
 
 export const getNonce = (event: PageEvent | RequestEvent | { nonce: string }) =>
     (event as unknown as { nonce: string }).nonce;
