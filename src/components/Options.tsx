71d9ea8b5eb89238e094edf9b286d7e06686fd57
Ben Bergenwall <ben.bergenwall@gmail.com>
2025-02-15 03:14:39 +0200

    Continue working on Options

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,10 +1,22 @@
-export type OptionsProps = {
-    class?: string | undefined;
-};
+const OptionsContents = () => (
+    <fieldset class='m-0 hidden h-0 w-0 p-0 peer-has-checked:block'>
+        <legend>Settings</legend>
+        WOW
+    </fieldset>
+);
 
-const Options = (props: OptionsProps) => (
-    <div class={`fixed top-60 left-0 ${props.class ?? ''}`}>
-        Options Oh Boy!
+const Options = () => (
+    <div class='fixed inset-y-1/2 m-0 p-0'>
+        <div class='peer m-0 p-0'>
+            <input class='hidden' type='checkbox' id='options-hamburger' />
+            <label
+                class='m-0 rotate-180 cursor-pointer bg-gray-500 px-3 py-0 [writing-mode:vertical-lr]'
+                for='options-hamburger'
+            >
+                Options
+            </label>
+        </div>
+        <OptionsContents />
     </div>
 );
 
